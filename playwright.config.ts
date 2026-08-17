c567b616a7c947f6cb823b34069770ff90fb4b15
ambuj yadav <ambujyadav201295@gmail.com>
2026-08-17 23:29:01 +0530

    update file playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
    
     // Browser setting
-   headless:false,
+   headless:true,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
